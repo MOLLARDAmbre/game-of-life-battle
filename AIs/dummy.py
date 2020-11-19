8a202e5f8fb495bcca7c797507d33c5e4ceba007
MOLLARD Ambre <samuelle.mollard@imt-atlantique.net>
2020-11-19 13:58:37 +0100

    new file:   AIs/dummy.py 	new file:   utils.py Added util files Added an AI that does nothing

---
+++ AIs/dummy.py
@@ -0,0 +1,23 @@
+from cell import Cell
+from random import randint
+
+class Player():
+
+    def preprocessing(self, board, player):
+        """
+        This function allows you to have time to preprocess stuff before the game starts
+        :param board: 2D array of cells
+        :param player: Either Cell.PLAYER1 or Cell.PLAYER2 depending on which player you are
+        :return: None
+        """
+        return None
+
+    def turn(self, board, player):
+        """
+        This is the turn function
+        You should return one value which will either be a kill or a spawn of a cell depending on what the board contains at the index
+        :param board: 2D array of cells
+        :param player: Either Cell.PLAYER1 or Cell.PLAYER2 depending on which player you are
+        :return: (x, y) tuple with the coordinates of the cell to change
+        """
+        return None
---
+++ utils.py
@@ -0,0 +1,17 @@
+"""
+This files explicitly exposes utility functions
+"""
+
+from cell import Cell
+from cellular_automata import next
+
+
+def other_player(cell_color):
+    if (cell_color == Cell.PLAYER1):
+        return Cell.PLAYER2
+    if (cell_color == Cell.PLAYER2):
+        return Cell.PLAYER1
+    return None # Should never be reached
+
+def next_board_state(board):
+    return cellular_automata.next(board)
